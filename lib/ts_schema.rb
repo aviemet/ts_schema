0c56b9374c5f3a53af337bdedda1942b1173743f
Avram Walden <aviemet@gmail.com>
2022-04-05 12:35:05 -0700

    Aborts generating schema if there are pending migrations

--- lib/ts_schema.rb
+++ lib/ts_schema.rb
@@ -4,6 +4,7 @@ require "ts_schema/railtie"
 require "ts_schema/configuration"
 require "ts_schema/schema_generator"
 
+
 module TsSchema
   class << self
     def setup(&block)
@@ -15,11 +16,19 @@ module TsSchema
     end
 
     def generate
-      SchemaGenerator.new.generate
+      if ActiveRecord::Base.connection.migration_context.needs_migration?
+        puts "Aborting: There are pending migrations"
+      else 
+        SchemaGenerator.new.generate
+      end
     end
 
 		def output_file
-			SchemaGenerator.new.output_file
+      if ActiveRecord::Base.connection.migration_context.needs_migration?
+        puts "Aborting: There are pending migrations"
+      else 
+        SchemaGenerator.new.output_file
+      end
 		end
   end
 end
